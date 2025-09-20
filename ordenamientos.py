04408ebb008fe4b735fab9a30d35491c0826dd1f
bojorquezja <bojorquez.ja@gmail.com>
2025-09-19 20:49:15 -0500

    ordenamiento 2

--- ordenamientos.py
+++ ordenamientos.py
@@ -6,7 +6,12 @@ def ordsel(lista):
                 posMen = vista
         lista[manoIzq], lista[posMen] = lista[posMen], lista[manoIzq]
 
+def ordbur(lista):
+    for vueltas in range(len(lista)-1):
+        for pos1par in range(len(lista)-1-vueltas):
+            if lista[pos1par] > lista[pos1par+1]:
+                lista[pos1par], lista[pos1par+1] = lista[pos1par+1], lista[pos1par]
 
 numeros = [12, 11, 13, 15, 6, 9, 30, 26, 28, 18, 8, 2, 20, 5, 25]
-ordsel(numeros)
+ordbur(numeros)
 print(numeros)
